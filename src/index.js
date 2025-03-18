07bceb9bdbf3476394544010ff833d37a7ac62a5
Romanov <vit04rom@mail.ru>
2025-03-18 19:30:26 +0500

    ФТ-201 Романов Кускова

--- src/index.js
+++ src/index.js
@@ -2,14 +2,86 @@ import Card from './Card.js';
 import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
+import card from "./Card.js";
+
+
+class Creature extends Card {
+    constructor(name, maxPower) {
+        super(name, maxPower);
+    }
+
+    getDescriptions(card){
+        return [getCreatureDescription(card),
+            ...super.getDescriptions()];
+    }
+}
+
+class Duck extends Creature {
+    constructor() {
+        super("Мирная утка", 2);
+    }
+
+    quacks() {
+        console.log('quack');
+    }
+
+    swims() {
+        console.log('float: both;');
+    }
+}
+
+class Dog extends Creature {
+    constructor() {
+        super("Пес бандит", 2);
+    }
+}
+
+class Trasher extends Dog {
+    constructor() {
+        super();
+        this.name = "Громила";
+        this.maxPower = 5;
+        this.currentPower = 5;
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation){
+        this.view.signalAbility(() => continuation(value - 1));
+    }
+
+    getDescriptions() {
+        return ["Уменьшение входящего урона на 1",
+            ...super.getDescriptions()];
+    }
+}
+
+class Gatling extends Creature {
+    constructor() {
+        super("Гатлинг", 6);
+    }
+
+    attack(gameContext, continuation) {
+
+        const oppositeCards = gameContext.oppositePlayer.table;
+
+        if (oppositeCards.length > 0) {
+            for (const oppositeCard of oppositeCards) {
+                super.dealDamageToCreature(2, oppositeCard, gameContext);
+                setTimeout(1)
+            }
+        }
+    }
+}
+
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
+    console.log(card && card.quacks && card.swims);
     return card && card.quacks && card.swims;
 }
 
 // Отвечает является ли карта собакой.
 function isDog(card) {
+    console.log(card instanceof Dog);
     return card instanceof Dog;
 }
 
@@ -28,29 +100,19 @@ function getCreatureDescription(card) {
 }
 
 
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
-
-
-// Основа для собаки.
-function Dog() {
-}
-
-
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
+    new Gatling(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 
 
@@ -64,3 +126,5 @@ SpeedRate.set(1);
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
+
+
